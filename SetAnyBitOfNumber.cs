b772fdd866cb1f4415722a1f454e3a7f71f8bd49
shivamdhingra <shivam216174@gmail.com>
2023-02-21 14:24:38 +0530

    SetAnyBitOfNumber

---
+++ SetAnyBitOfNumber.cs
@@ -0,0 +1,10 @@
+            //Input
+            int n = 64, b = 2;
+            Console.WriteLine(SetAnyBitOfNumber(n, b));
+            Console.ReadKey(); 
+ 
+        public static int SetAnyBitOfNumber(int number , int b)
+        {
+            //right shift 1 to b-1 place and X-Or with the number will set the bit on that position
+            return number^ (1<<b-1);
+        }
